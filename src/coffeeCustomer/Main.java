7265ce32e25531ef7f5e95fbe371d8a1f3c2fa25
Mustafa Çağrı ALTINDAL <mcaltindal@cagrialtindal.com.tr>
2021-07-31 01:37:50 +0300

    service changeds.

--- src/coffeeCustomer/Main.java
+++ src/coffeeCustomer/Main.java
@@ -1,14 +1,15 @@
 package coffeeCustomer;
 
 
+import coffeeCustomer.abstracts.CustomerService;
 import coffeeCustomer.concrete.NeroCustomerManager;
 import coffeeCustomer.entities.Customer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		NeroCustomerManager neroCustomerManager = new NeroCustomerManager();
-		neroCustomerManager.save(new Customer(1, "Mustafa Çaðrý", "Altýndal", 37, "12345678911"));
+		CustomerService customerService = new NeroCustomerManager();
+		customerService .save(new Customer(1, "Mustafa Çaðrý", "Altýndal", 37, "12345678911"));
 	}
 
 }
